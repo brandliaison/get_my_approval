9a7c88816babc65edf5d6c5aa90f4b2e63c1e1cc
devloper1bl <devloper1.bl@gmail.com>
2025-03-25 14:25:21 +0530

    service partner registration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\IT\WebsiteSetting;
 use App\Models\OP\Blog;
 use App\Models\OP\TutorialVideo;
+use App\Models\ServicePartner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -23,5 +25,41 @@ class HomeController extends Controller
     {
         $data = WebsiteSetting::get();
         return response()->json(['data' => $data], 200);
+    }
+    public function registerServicePartner(Request $request)
+    {
+        $rules = [
+            'country' => 'required|in:India,Other',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:service_partners,email',
+            'mobile' => 'required|digits:10|unique:service_partners,mobile',
+            'alt_mobile' => 'nullable|digits:10',
+            'state' => 'required|string',
+            'district' => 'required|string',
+            'pincode' => 'required|string|max:10',
+        ];
+
+        // Validate the request
+        $validator = Validator::make($request->all(), $rules);
+
+        // Create user
+        $user = ServicePartner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'mobile_number' => $request->mobile_number,
+            'alt_mobile' => $request->alt_mobile,
+            'country' => $request->country,
+            'state' => $request->state,
+            'district' => $request->district,
+            'pincode' => $request->pincode,
+            'email_otp' => random_int(111111,999999),
+            'mobile_otp' => random_int(111111,999999),
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Otp Sent On Email And Mobile',
+            'user_id' => $user
+        ], 201);
     }
 }
